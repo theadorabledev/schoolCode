1d20506bb8938e61ec9a5a1cf4c5626debf57c47
unknown <norbekian9@gmail.com>
2018-09-25 22:11:38 -0400

    Can now perform arbitrarily small operations.

--- timeIt.scm
+++ timeIt.scm
@@ -18,21 +18,35 @@
 
   )
 
-; (fib desiredLength)
+(define (recursive-call times func args)
+  (if (= times 1)
+      (apply func args)
+      (begin
+        (recursive-call (- times 1) func args)
+        (apply func args)
+        )
+      )
+  )
+
+;(recursive-call 5 fib '(10))
 
 (define (timeIt func args)
   (begin
     
     (define startTime (current-milliseconds))
 
-    (apply func args) (newline)
+    ;(apply func args) (newline)
+    (recursive-call 100000 func args)
     (define nowTime (current-milliseconds))
 
-    (define elapsedTime (- nowTime startTime))
+
+    (define elapsedTime (/ (- nowTime startTime) 1000))
+    (newline)
     (display "Times you can run in one second: " )
-    (display (round (/ 1 (/ elapsedTime 1000))))
+    (display (round (/ 1(/(/ (- nowTime startTime) 1000) 100000)))) ; get the time in milliseconds, turn it into seconds, divide by number of operations performed, turn it into a whole number
     )
   )
 
 ;Use: (timeIt function-name '(args for function) )
-;Example: (timeIt fib '(272))
+;Example: (timeIt fib '(12))   (timeIt + '(5 7))
+
