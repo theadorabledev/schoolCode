a3393e889391b993e1884713663feee1334ec993
ianStuy <norbekian9@gmail.com>
2018-10-01 11:49:32 -0400

    Awesomified sieve

--- sieve.py
+++ sieve.py
@@ -3,35 +3,29 @@ import time
 from math import sqrt
 def sieve(n):
     """ Main function that performs incremental sieve of eratosthenes to get nth prime. """
-    primes=[i for i in xrange(2,1000)]
+    primes=[True for i in xrange(2,1000)]
     accumulativeLength = len(primes)
     sieveSub(primes)
-    
-    last = primes[-1]
     while True:
-        if n > len(primes):
-            print len(primes)
-            #newLength = (n * accumulativeLength)/len(primes)
-            primes += [i for i in xrange(accumulativeLength, accumulativeLength * 2)]#accumulativeLength+newLength)]
-            print last, primes[accumulativeLength]
-            last=primes[-1]
+        if n > len([i for i, z in enumerate(primes) if z]):
+            primes += [True for i in xrange(2, accumulativeLength)]
             accumulativeLength *= 2
             sieveSub(primes)
         else:
             break
             
-        
-    print primes[n-1]
+    print  [i for i, z in enumerate(primes) if z][n+1] 
  
 def sieveSub(primes):
     """ Subprocess that performs the sieve operation. """
-    for i in primes:
-        for x in primes:
-            if (x % i == 0) and (x != i):
-                primes.remove(x)
+    accumulativeLength=len(primes)
+    for i in range(2,accumulativeLength):
+        if i:
+            for x in range(i**2, accumulativeLength, i):
+                primes[x]=False
 
             
 if __name__ == "__main__":
     x=time.time()
-    sieve(10000)
+    sieve(1000000)
     print time.time()-x
