112da1caaeeaf0ca75b9612d95f88fb7677aa8e5
Ian Williams <norbekian9@gmail.com>
2018-10-03 19:34:26 -0400

    I'm sexy and I know it

--- sieve.py
+++ sieve.py
@@ -7,27 +7,21 @@ def sieve(n):
     	primes=[True for i in xrange(2,1002)]
     else:
 	primes=[True for i in xrange(2, pi(n)/2)]
-    accumulativeLength = len(primes)
     sieveSub(primes)
+    
 
-    #print primes        
-    print  [i for i, z in enumerate(primes) if z][n+1] 
+    x=time.time()
  
+    print  ([i for i, z in enumerate(primes) if z][n-2] * 2) + 3
+
+    print time.time()-x 
 def sieveSub(primes):
     """ Subprocess that performs the sieve operation. """
     accumulativeLength=len(primes)
-    for i in range(accumulativeLength):
-        raw_input("->")
-        print i, ((2*i)+3), primes[i]
-        print primes[:20]
-        #print [i for i, z in enumerate(primes[:20])]
+    for i in range(int(sqrt(accumulativeLength))):
         if primes[i]:
-            print ((2 * i)+3, accumulativeLength, (2 * i)+3)
-            for x in range((2 * i)+3, accumulativeLength, (2 * i)+3):
-                #print x
+            for x in range((((2 * i)+3) ** 2 -3 )/2, accumulativeLength, (2 * i)+3):
                 primes[x]=False
-        print [str((2*i +3 )) + str(primes[i]) for i in range(20)]
-        #print [i for i, z in enumerate(primes[:20])]
 
 
         
@@ -36,10 +30,10 @@ def sieveSub(primes):
 def pi(n):
     """ Returns apprximatio of what nth prime will be. """
     #https://en.wikipedia.org/wiki/Prime_number_theorem#Approximations_for_the_nth_prime_number
-    return int((n * (log(n) + log(log(n)) -1 + ((log(log(n)) -2 )/log(n)) -( ((log(log(n)) ** 2) - (6 * log(log(n))) + 11 ) /(2* (log(n) ** 2)))  + (1/ (log(n) **2)))) * 1.01)
+    return int((n * (log(n) + log(log(n)) -1 + ((log(log(n)) -2 )/log(n)) -( ((log(log(n)) ** 2) - (6 * log(log(n))) + 11 ) /(2* (log(n) ** 2)))  + (1/ (log(n) **2)))) * 1.05)
 if __name__ == "__main__":
     x=time.time()
-    sieve(1000)
+    sieve(1000000)
     print time.time()-x
     
     
