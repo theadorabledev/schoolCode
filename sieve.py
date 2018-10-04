ec7a81375c9b5d5103e4d795b4bf4cebf07e3bdb
ianStuy <norbekian9@gmail.com>
2018-10-04 11:50:03 -0400

    Now uses command line

--- sieve.py
+++ sieve.py
@@ -1,27 +1,25 @@
 #! /usr/bin/python2.7
 import time
 from math import sqrt, log
+import sys
 def sieve(n):
     """ Main function that performs incremental sieve of eratosthenes to get nth prime. """
-    if n < 100:
-    	primes=[True for i in xrange(2,1002)]
+    if n == 1:
+        return 2
+    elif n < 200:
+    	primes = [True for i in xrange(2,1002)]
     else:
-	primes=[True for i in xrange(2, pi(n)/2)]
-    sieveSub(primes)
-    
-
-    x=time.time()
- 
-    print  ([i for i, z in enumerate(primes) if z][n-2] * 2) + 3
-
-    print time.time()-x 
+	primes = [True for i in xrange(2, pi(n)/2)] # pi(n) is divided by 2 becuase even numbers are automatically not prime
+    sieveSub(primes) 
+    return  ([i for i, z in enumerate(primes) if z][n-2] * 2) + 3
 def sieveSub(primes):
     """ Subprocess that performs the sieve operation. """
     accumulativeLength=len(primes)
     for i in range(int(sqrt(accumulativeLength))):
         if primes[i]:
-            for x in range((((2 * i)+3) ** 2 -3 )/2, accumulativeLength, (2 * i)+3):
-                primes[x]=False
+            #((2 * i) + 3) is converting between indexes and odd number based system
+            for x in range((((2 * i) + 3) ** 2 - 3) / 2, accumulativeLength, (2 * i)+3):
+                primes[x] = False
 
 
         
@@ -32,8 +30,13 @@ def pi(n):
     #https://en.wikipedia.org/wiki/Prime_number_theorem#Approximations_for_the_nth_prime_number
     return int((n * (log(n) + log(log(n)) -1 + ((log(log(n)) -2 )/log(n)) -( ((log(log(n)) ** 2) - (6 * log(log(n))) + 11 ) /(2* (log(n) ** 2)))  + (1/ (log(n) **2)))) * 1.05)
 if __name__ == "__main__":
-    x=time.time()
-    sieve(1000000)
-    print time.time()-x
+    args = sys.argv[1:]
+    for i in args:
+        try:
+            x = time.time()
+            print "Prime number", str(i) ,"is",str(sieve(int(i)))
+            print "Calculated in", str(time.time() - x), "seconds.\n"
+        except ValueError:
+            pass
     
     
