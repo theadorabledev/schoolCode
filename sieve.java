1449ecda35e8974852fb61f58628c23236935950
Ian Williams <norbekian9@gmail.com>
2018-10-04 20:34:24 -0400

    Sieve for nth prime in java created in an hour.

---
+++ sieve.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.lang.Math; 
+import java.util.Arrays;
+
+public class sieve{
+	public static void main(String[] args){
+		long start = System.nanoTime(); 
+		int n = Integer.valueOf( args[0]) ;
+		if (n == 1){
+			System.out.println(2);
+			return;
+		}else{
+			boolean [] primes ;
+			int primesLength;
+			if (n < 200){
+				primesLength = (int) 1000;
+			}else{
+				primesLength = (int) pi(n)/2;
+			}
+			primes = new boolean [primesLength];
+			Arrays.fill(primes, true);
+			for(int i = 0; i < Math.sqrt(primesLength); i++){
+				if(primes[i]){
+					for(int x = (int) (Math.pow(((2 * i) + 3), 2) - 3) / 2; x < primesLength; x += ((2 * i) + 3)){
+						primes[x] = false;
+					}					
+				}
+			}
+			int p = (int) 1;
+			for(int i = 0; i < primesLength; i++){
+				if(primes[i]){
+					p += 1;
+					if(p == n){
+						System.out.println("Prime number "+ n + " is " + ((2 * i) + 3));
+						System.out.println("Calculated in " + ((System.nanoTime() - start) / 1000000000.0 ) + " seconds.");
+						break;
+					}
+				}
+			}
+		}
+
+	}
+	static int pi(int num) {
+		double n = num;
+		return (int) ((n * (Math.log(n) + Math.log(Math.log(n)) -1 + ((Math.log(Math.log(n)) -2 )/Math.log(n)) -( ((Math.pow(Math.log(Math.log(n)), 2)) - (6 * Math.log(Math.log(n))) + 11 ) /(2* Math.pow(Math.log(n), 2)))  + (1/ Math.pow(Math.log(n), 2)))) * 1.05);
+	}
+}
