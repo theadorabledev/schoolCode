76f254e61e7b1e5b7b304a6380986615e3f3bcb0
Theadora Williams <theadorabledeveloper@gmail.com>
2018-10-07 12:45:43 -0400

    Worked on sieve.

--- sieve.py
+++ sieve.py
@@ -7,11 +7,14 @@ def sieve(n):
     if n == 1:
         return 2
     elif n < 200:
-    	primes = [True for i in xrange(2,1002)]
-    else:
-	primes = [True for i in xrange(2, pi(n)/2)] # pi(n) is divided by 2 becuase even numbers are automatically not prime
-    sieveSub(primes) 
-    return  ([i for i, z in enumerate(primes) if z][n-2] * 2) + 3
+    	primes = [True] * 1000
+    else:	
+	primes = [True] * (pi(n)/2)# pi(n) is divided by 2 becuase even numbers are automatically not prime
+	sieveSub(primes)
+	#x = time.time()
+	#print test(primes, n)
+	#print time.time() - x
+    return test(primes, n)# ([i for i, z in enumerate(primes) if z][n-2] * 2) + 3
 def sieveSub(primes):
     """ Subprocess that performs the sieve operation. """
     accumulativeLength=len(primes)
@@ -29,6 +32,18 @@ def pi(n):
     """ Returns apprximatio of what nth prime will be. """
     #https://en.wikipedia.org/wiki/Prime_number_theorem#Approximations_for_the_nth_prime_number
     return int((n * (log(n) + log(log(n)) -1 + ((log(log(n)) -2 )/log(n)) -( ((log(log(n)) ** 2) - (6 * log(log(n))) + 11 ) /(2* (log(n) ** 2)))  + (1/ (log(n) **2)))) * 1.05)
+
+    
+def test(primes, n):
+	l = len(primes)
+	numPrimes = primes.count(True)
+	calc = numPrimes - n
+	for i in range(l-1 , 0, -1):
+		if primes[i]:
+			calc -= 1
+		if calc == -2:
+			return (2*i)+3
+
 if __name__ == "__main__":
     args = sys.argv[1:]
     for i in args:
@@ -38,5 +53,4 @@ if __name__ == "__main__":
             print "Calculated in", str(time.time() - x), "seconds.\n"
         except ValueError:
             pass
-    
     
