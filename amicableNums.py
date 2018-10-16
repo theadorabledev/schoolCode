dc55ee208065fdb4c2e1bb5e02e1da3de464bff1
Theadora Williams <theadorabledeveloper@gmail.com>
2018-10-16 11:50:54 -0400

    Worked on sociable  numbers.

--- amicableNums.py
+++ amicableNums.py
@@ -9,20 +9,40 @@ def findAmicableNums(start, upTo):
         if i != theSum and i == primeFactorsSum(theSum, theSum%2):
             nums.add(tuple(sorted((i, theSum))))
     return nums
+
 def test(num):
     theSum = primeFactorsSum(num, num%2)
     if num != theSum and num == primeFactorsSum(theSum, theSum%2):
         return tuple(sorted((num, theSum)))  
+def sociableNumTest(num, length=4):
+    """ Checks if the number is a sociable number with length under 4. """
+    nums = [primeFactorsSum(num, num%2)]
+    for i in xrange(length):
 
+        nums.append(primeFactorsSum(nums[-1],nums[-1]%2))
+        if nums[0] == nums[-1]:
+            return nums
+    return None
 def primeFactorsSum(n, increase):
+    """ Returns the sum of the factors. """
     return sum(set(reduce(list.__add__, ([i, n/i] for i in xrange(1, int(sqrt(n))+1, increase+1) if (n % i == 0))))) - n
     #return {i for i in xrange(1, n) if (n % i == 0) }
 def main():
     args = sys.argv[1:]
-    p = Pool()
-    x = set(p.map(test, range(2, int(args[0]))))
-    x.remove(None)
-    print sorted(x)
+    if args[0] != "--sociable":
+        
+        p = Pool()
+        x = set(p.map(test, range(2, int(args[0]))))
+        x.remove(None)
+        print sorted(x)
+    else:
+        for i in xrange(2,int(args[1])):
+            print sociableNumTest(i)
+        #p = Pool()
+        #x = set(p.map(sociableNumTest, range(2, int(args[1]))))
+        #x.remove(None)
+        #print sorted(x)
+        #print sociableNumTest(int(args[1]))
     #print findAmicableNums(2, int(args[0]))
 if __name__ == "__main__":
     x = time.time()
