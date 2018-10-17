575fa6fd1e584b29de5802fd90536a01cdeca4c6
Theadora Williams <theadorabledeveloper@gmail.com>
2018-10-17 11:49:47 -0400

    Lookup table for amicable nums.

--- amicableNums.py
+++ amicableNums.py
@@ -1,12 +1,30 @@
 import sys
 from math import sqrt
 import time
+import json
 from multiprocessing import Pool
+
+def setupFactorFile(upTo):
+    try:
+        rfile = open("factors.json", "r")
+        fileJSON = json.load(rfile) 
+    except IOError:
+        with open("factors.json", "w") as file:
+            json.dump({i:primeFactorsSum(i, i%2) for i in xrange(1, upTo + 1)}, file)
+    else:    
+        if len(fileJSON) < upTo:
+            for i in xrange(len(fileJSON), upTo+1):
+                fileJSON[i] = primeFactorsSum(i, i%2)
+        with open("factors.json", "w") as file:
+            json.dump(fileJSON, file)
+    with open("factors.json", "r") as file:
+        return json.load(file) 
+
 def findAmicableNums(start, upTo):
     nums = set()
     for i in xrange(start, upTo):
-        theSum = primeFactorsSum(i, i%2)
-        if i != theSum and i == primeFactorsSum(theSum, theSum%2):
+        theSum = data[str(i)]
+        if i != theSum and i == data[str(theSum)]:
             nums.add(tuple(sorted((i, theSum))))
     return nums
 
@@ -14,10 +32,11 @@ def test(num):
     theSum = primeFactorsSum(num, num%2)
     if num != theSum and num == primeFactorsSum(theSum, theSum%2):
         return tuple(sorted((num, theSum)))  
+
 def sociableNumTest(num, length=4):
     """ Checks if the number is a sociable number with length under 4. """
     nums = [primeFactorsSum(num, num%2)]
-    for i in xrange(length):
+    for i in xrange( length):
 
         nums.append(primeFactorsSum(nums[-1],nums[-1]%2))
         if nums[0] == nums[-1]:
@@ -26,24 +45,23 @@ def sociableNumTest(num, length=4):
 def primeFactorsSum(n, increase):
     """ Returns the sum of the factors. """
     return sum(set(reduce(list.__add__, ([i, n/i] for i in xrange(1, int(sqrt(n))+1, increase+1) if (n % i == 0))))) - n
-    #return {i for i in xrange(1, n) if (n % i == 0) }
 def main():
     args = sys.argv[1:]
+    global factors
     if args[0] != "--sociable":
-        
+        factors = setupFactorFile(int(args[0]))
         p = Pool()
         x = set(p.map(test, range(2, int(args[0]))))
         x.remove(None)
         print sorted(x)
     else:
-        for i in xrange(2,int(args[1])):
-            print sociableNumTest(i)
-        #p = Pool()
-        #x = set(p.map(sociableNumTest, range(2, int(args[1]))))
-        #x.remove(None)
-        #print sorted(x)
-        #print sociableNumTest(int(args[1]))
-    #print findAmicableNums(2, int(args[0]))
+        factors = setupFactorFile(int(args[1]))
+        p = Pool()
+        x = set(p.map(sociableNumTest, range(2, int(args[1]))))
+        x.remove(None)
+        print sorted(x)
+
+
 if __name__ == "__main__":
     x = time.time()
     main()
