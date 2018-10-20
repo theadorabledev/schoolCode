91b3de64e0e1d07723dcef11aac5619f5a541afd
Ian Williams <norbekian9@gmail.com>
2018-10-19 23:12:07 -0400

    Wordladder search algorithm.

--- neighbors.py
+++ neighbors.py
@@ -7,16 +7,20 @@ def main():
     global theDictionary
     theDictionary = set(dictAll.read().splitlines())
     #print theDictionary
-    getNeighbors(args[0])
+    connections = returnDict(args[0])
 def getNeighbors(word):
+    words = set()
     count = 0
     for i in xrange(len(word)):
         temp = ""
         for letter in ALPHABET:
             if letter != word[i] and (word[:i] + letter + word[i+1:]) in theDictionary:
                 count += 1
-    print count
+                words.add(word[:i] + letter + word[i+1:])
+    #print count
+    return words
     
-    
+def returnDict(argWord):
+    return {word:getNeighbors(word) for word in theDictionary if len(word)==len(argWord)}
 if __name__ == "__main__":
     main()
