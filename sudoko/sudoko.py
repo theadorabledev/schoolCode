9b98d8de436ef5e39f826fa51b92465c07fa82b7
Theadora Williams <theadorabledeveloper@gmail.com>
2018-11-08 11:49:02 -0500

    To much man.

--- sudoko/sudoko.py
+++ sudoko/sudoko.py
@@ -1,5 +1,6 @@
 import sys
 class SudokoPuzzle:
+    #getGroup = lambda x, y:((x + 3)/3) + ((y/3) * 3)
     def __init__(self, fileName):
         self.data = []
         self.getGroup = lambda x, y:((x + 3)/3) + ((y/3) * 3)
@@ -8,6 +9,7 @@ class SudokoPuzzle:
         self.unknownValues = set()
         self.countValues = {num:0 for num in xrange(1, 10)}
         self.knownCount = 0
+        self.savedState = None
         lines = open(fileName).readlines()
         for line in lines:
             self.data.append([int(i) for i in line.strip().replace("_", "0").split(",")])
@@ -17,11 +19,21 @@ class SudokoPuzzle:
         self.columns = {col:set(row[col] for row in self.data) for col in xrange(9)}
         self.groups = {group:set() for group in xrange(1, 10)}
         self.initGroups()
+        self.solve()
+    def solve(self):
         while self.knownCount < 80:
+            lastCount = self.knownCount
             self.updateValues()
-        
-  #      p 
-        #self.printData()
+            if lastCount == self.knownCount:
+                #print self.__dict__
+                self.printData()
+                for v in self.possibleValues:
+                    if len(self.possibleValues[v]) > 1:
+                        print v, self.possibleValues[v]
+                m = min(self.possibleValues, key=lambda k: len(self.possibleValues[k]) if len(self.possibleValues[k]) > 1 else 10)
+                print m, self.possibleValues[m]
+                raw_input("-->")       
+    #random
     def initValues(self):
         for x in xrange(9):
             for y in xrange(9):
@@ -30,6 +42,8 @@ class SudokoPuzzle:
                     self.knownValues.add((x, y))
                     self.countValues[self.data[y][x]] += 1
                     self.knownCount += 1
+    def saveState(self):
+        self.savedState = self.__dict__.copy()
     def printData(self):
         for line in self.data:
             print " ".join([str(i) for i in line])  
