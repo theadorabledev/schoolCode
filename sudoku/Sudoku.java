56749c74839c24fbd9e0289c5c225f693aee2770
Theadora Williams <theadorabledeveloper@gmail.com>
2018-12-05 11:50:25 -0500

    Created grid.

--- sudoku/Sudoku.java
+++ sudoku/Sudoku.java
@@ -9,9 +9,11 @@ public class Sudoku{
         JPanel buttonPanel = new JPanel();
         JPanel containerPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(9,9));
-        for(int i = 1; i <= 81; i ++){
-			buttonPanel.add(new JButton(String.valueOf(i)));
-		}
+        for(int y = 0; y < 9; y++){
+	    for(int x = 0; x < 9; x ++){
+		buttonPanel.add(new SudokuButton(new Coordinate(x, y)));
+	    }
+	}
 		//buttonPanel.add(new JButton("1"));
         //buttonPanel.add(new JButton("2"));
         //buttonPanel.add(new JButton("3"));
