15f0f9cbf5e9b55125f9eff2c31513986c352dbf
Theadora Williams <theadorabledeveloper@gmail.com>
2018-12-06 11:50:20 -0500

    More work on it.

--- sudoku/Sudoku.java
+++ sudoku/Sudoku.java
@@ -10,21 +10,33 @@ public class Sudoku{
 	private JPanel control = new JPanel();
 	private boolean pieceClicked;
 	private SudokuButton currentCoordinate = null;
+        private JButton generatePuzzleButton = new JButton("Generate Puzzle");
+    private SudokuPuzzle myPuzzle;
+    private HashMap<Coordinate, SudokuButton> gridButtons = new HashMap<Coordinate, SudokuButton>();
 	public Sudoku(){
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
+			
         gridPanel.setLayout(new GridLayout(9,9));
         for(int y = 0; y < 9; y++){
 			for(int x = 0; x < 9; x ++){
-				gridPanel.add(new SudokuButton(new Coordinate(x, y),this));
+			    SudokuButton b = new SudokuButton(new Coordinate(x, y),this);
+				gridPanel.add(b);
+				gridButtons.put(b.coord, b);
 			}
 		}
 		gridPanel.setBorder(BorderFactory.createMatteBorder(5, 5, 5, 5, Color.blue));
         gridPanel.setPreferredSize(new Dimension(500, 500));
         gridContainer.add(gridPanel);
 		gridContainer.setPreferredSize(new Dimension(700, 700));
-		control.add(new JButton("hello"));
+		control.add(generatePuzzleButton);
+		generatePuzzleButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			    myPuzzle = new SudokuPuzzleGenerated("Hard");
+			    loadPuzzleData();
+			}
+		    });
         //frame.getContentPane().add(gridContainer);
         
 		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, control, gridContainer);
@@ -44,4 +56,13 @@ public class Sudoku{
 		//b.setText("Yep");
 		b.setBackground(Color.green);
 	}
+    public void loadPuzzleData(){
+	Integer[][] myData = myPuzzle.data;
+	myPuzzle.printData();
+	for(int x = 0; x < 9; x ++){
+	    for(int y = 0; y < 9; y++){
+		gridButtons.get(new Coordinate(x, y)).setText(String.valueOf(myData[y][x]));
+	    }
+	}
+    }
 }
