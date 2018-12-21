d0f8ec0f9bb810d7d11f1ac22db3b4be829cac93
Theadora Williams <theadorabledeveloper@gmail.com>
2018-12-20 20:36:09 -0500

    Now have a working netlogo extension. Next step, making it actually do anything useful.

--- sudoku/SudokuJava/setupNetlogoJar.sh
+++ sudoku/SudokuJava/setupNetlogoJar.sh
@@ -1,4 +1,8 @@
 #!/bin/bash  
 
-javac -classpath /usr/local/netlogo-6.0.1/app/netlogo-6.0.1.jar -d target/bin/nlogoClasses src/main/java/NetlogoJava/*.java
-jar cvfm target/SudokuAppNetlogo.jar src/main/resources/manifest.txt -C target/bin/nlogoClasses .
+mkdir target/bin/nlogoClasses
+mkdir target/sudoku-netlogo
+javac -cp src/main/resources/netlogo-6.0.4.jar;src/main/resources/scala-library.jar -d target/bin/nlogoClasses src/main/java/NetlogoJava/*.java
+jar cvfm target/sudoku-netlogo/sudoku-netlogo.jar src/main/resources/manifest.txt -C target/bin/nlogoClasses .
+chmod +x target/sudoku-netlogo/sudoku-netlogo.jar
+$SHELL
