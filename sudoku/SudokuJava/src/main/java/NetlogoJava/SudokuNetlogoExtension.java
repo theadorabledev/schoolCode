ae163057ba4da216b844c3931afdb5d9e3c0ecb2
unknown <norbekian9@gmail.com>
2018-12-21 00:04:39 -0500

    Can now report (Fixed value) generated sudoku data, but can not yet pass creation args.

--- sudoku/SudokuJava/src/main/java/NetlogoJava/SudokuNetlogoExtension.java
+++ sudoku/SudokuJava/src/main/java/NetlogoJava/SudokuNetlogoExtension.java
@@ -27,15 +27,44 @@ public class SudokuNetlogoExtension extends DefaultClassManager {
 			}
 		// populate the list. note that we use Double objects; NetLogo
 		// numbers are always Doubles
-		for (int i = 0; i < n; i++) {
-			list.add(Double.valueOf(i));
+			for (int i = 0; i < n; i++) {
+				list.add(Double.valueOf(i));
+			}
+			return list.toLogoList();
 		}
-		return list.toLogoList();
+	}
+	
+	public class PuzzleList implements Reporter {
+		public Syntax getSyntax() {
+			return SyntaxJ.reporterSyntax(new int[] {Syntax.NumberType()}, Syntax.ListType());
+		}
+		public Object report(Argument args[], Context context) throws ExtensionException {
+			// create a NetLogo list for the result
+			LogoListBuilder list = new LogoListBuilder();
+			SudokuPuzzleGenerated s;
+			int n ;
+			// use typesafe helper method from
+			// org.nlogo.api.Argument to access argument
+			try {
+				n = args[0].getIntValue();
+				s = new SudokuPuzzleGenerated("Easy", "Seed");
+			}catch(LogoException e) {
+				throw new ExtensionException( e.getMessage() ) ;
+			}
+			
+			Integer [][] data = s.getData();
+			for (int i = 0; i < 9; i++) {
+				for(int k = 0; k < 9; k++){
+					list.add(Double.valueOf(data[i][k]));
+				}
+			}
+			return list.toLogoList();
 		}
 	}
 	//http://ccl.northwestern.edu/netlogo/2.1/docs/extensions.html
     //https://github.com/NetLogo/NetLogo/wiki/Extensions-API
     public void load(PrimitiveManager pm){
-	pm.addPrimitive("blueFish", new IntegerList());
+		pm.addPrimitive("blueFish", new IntegerList());
+		pm.addPrimitive("generate-puzzle", new PuzzleList());
     }
 }
