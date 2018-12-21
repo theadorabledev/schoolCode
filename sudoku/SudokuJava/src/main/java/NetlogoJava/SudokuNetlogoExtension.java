55628b49faeb10f87b9bc43aa02eba952d0c4948
unknown <norbekian9@gmail.com>
2018-12-20 20:36:09 -0500

    Now have a working netlogo extension. Next step, making it actually do anything useful.

--- sudoku/SudokuJava/src/main/java/NetlogoJava/SudokuNetlogoExtension.java
+++ sudoku/SudokuJava/src/main/java/NetlogoJava/SudokuNetlogoExtension.java
@@ -6,38 +6,34 @@ public class SudokuNetlogoExtension extends DefaultClassManager {
 
 
     public class IntegerList implements Reporter {
-	// take one number as input, report a list
-	public Syntax getSyntax() {
-	    return SyntaxJ.reporterSyntax(
-					  new int[] {Syntax.NumberType()}, Syntax.ListType());
+		// take one number as input, report a list
+		public Syntax getSyntax() {
+			return SyntaxJ.reporterSyntax(new int[] {Syntax.NumberType()}, Syntax.ListType());
+		}
+		public Object report(Argument args[], Context context) throws ExtensionException {
+			// create a NetLogo list for the result
+			LogoListBuilder list = new LogoListBuilder();
+			int n ;
+			// use typesafe helper method from
+			// org.nlogo.api.Argument to access argument
+			try {
+				n = args[0].getIntValue();
+			}catch(LogoException e) {
+				throw new ExtensionException( e.getMessage() ) ;
+			}
+			if (n < 0) {
+				// signals a NetLogo runtime error to the modeler
+				throw new ExtensionException("input must be positive");
+			}
+		// populate the list. note that we use Double objects; NetLogo
+		// numbers are always Doubles
+		for (int i = 0; i < n; i++) {
+			list.add(Double.valueOf(i));
+		}
+		return list.toLogoList();
+		}
 	}
-	public Object report(Argument args[], Context context)
-	    throws ExtensionException {
-	    // create a NetLogo list for the result
-	    LogoListBuilder list = new LogoListBuilder();
-	    int n ;
-	    // use typesafe helper method from
-	    // org.nlogo.api.Argument to access argument
-	    try {
-		n = args[0].getIntValue();
-	    }
-	    catch(LogoException e) {
-		throw new ExtensionException( e.getMessage() ) ;
-	    }
-	    if (n < 0) {
-		// signals a NetLogo runtime error to the modeler
-		throw new ExtensionException
-		    ("input must be positive");
-	    }
-	    // populate the list. note that we use Double objects; NetLogo
-	    // numbers are always Doubles
-	    for (int i = 0; i < n; i++) {
-		list.add(Double.valueOf(i));
-	    }
-	    return list.toLogoList();
-	}
-    }
-    //http://ccl.northwestern.edu/netlogo/2.1/docs/extensions.html
+	//http://ccl.northwestern.edu/netlogo/2.1/docs/extensions.html
     //https://github.com/NetLogo/NetLogo/wiki/Extensions-API
     public void load(PrimitiveManager pm){
 	pm.addPrimitive("blueFish", new IntegerList());
