84023375c22dd8cce7e1946c9c7eb894b357c73b
unknown <norbekian9@gmail.com>
2018-12-22 09:23:12 -0500

    Last night I fixed the netlogo extension so that it now returns generated puzzles based on passed arguments.

--- sudoku/SudokuJava/src/main/java/NetlogoJava/SudokuNetlogoExtension.java
+++ sudoku/SudokuJava/src/main/java/NetlogoJava/SudokuNetlogoExtension.java
@@ -5,38 +5,11 @@ import org.nlogo.core.SyntaxJ;
 public class SudokuNetlogoExtension extends DefaultClassManager {
 
 
-    public class IntegerList implements Reporter {
-		// take one number as input, report a list
-		public Syntax getSyntax() {
-			return SyntaxJ.reporterSyntax(new int[] {Syntax.NumberType()}, Syntax.ListType());
-		}
-		public Object report(Argument args[], Context context) throws ExtensionException {
-			// create a NetLogo list for the result
-			LogoListBuilder list = new LogoListBuilder();
-			int n ;
-			// use typesafe helper method from
-			// org.nlogo.api.Argument to access argument
-			try {
-				n = args[0].getIntValue();
-			}catch(LogoException e) {
-				throw new ExtensionException( e.getMessage() ) ;
-			}
-			if (n < 0) {
-				// signals a NetLogo runtime error to the modeler
-				throw new ExtensionException("input must be positive");
-			}
-		// populate the list. note that we use Double objects; NetLogo
-		// numbers are always Doubles
-			for (int i = 0; i < n; i++) {
-				list.add(Double.valueOf(i));
-			}
-			return list.toLogoList();
-		}
-	}
-	
+    
 	public class PuzzleList implements Reporter {
 		public Syntax getSyntax() {
-			return SyntaxJ.reporterSyntax(new int[] {Syntax.NumberType()}, Syntax.ListType());
+			//https://github.com/NetLogo/NetLogo/blob/5.1.0/src/main/org/nlogo/api/Syntax.scala
+			return SyntaxJ.reporterSyntax(new int[] {Syntax.StringType(), Syntax.StringType()}, Syntax.ListType());
 		}
 		public Object report(Argument args[], Context context) throws ExtensionException {
 			// create a NetLogo list for the result
@@ -46,8 +19,9 @@ public class SudokuNetlogoExtension extends DefaultClassManager {
 			// use typesafe helper method from
 			// org.nlogo.api.Argument to access argument
 			try {
-				n = args[0].getIntValue();
-				s = new SudokuPuzzleGenerated("Easy", "Seed");
+				//n = args[0].getIntValue();
+				s = new SudokuPuzzleGenerated(args[0].getString(), args[1].getString());
+				//System.out.println(args[0].getString() + args[1].getString());
 			}catch(LogoException e) {
 				throw new ExtensionException( e.getMessage() ) ;
 			}
@@ -64,7 +38,6 @@ public class SudokuNetlogoExtension extends DefaultClassManager {
 	//http://ccl.northwestern.edu/netlogo/2.1/docs/extensions.html
     //https://github.com/NetLogo/NetLogo/wiki/Extensions-API
     public void load(PrimitiveManager pm){
-		pm.addPrimitive("blueFish", new IntegerList());
 		pm.addPrimitive("generate-puzzle", new PuzzleList());
     }
 }
