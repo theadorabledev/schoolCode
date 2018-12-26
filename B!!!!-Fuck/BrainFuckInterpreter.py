a964984010d49c62658092f4e098a2ed764b5467
unknown <norbekian9@gmail.com>
2018-12-25 21:36:44 -0500

    I created a B****Fuck interpreter for files and passed arguments. It also parses Ook for librarian-coders at un-C-n universities.

---
+++ B!!!!-Fuck/BrainFuckInterpreter.py
@@ -0,0 +1,69 @@
+import argparse
+def findBraces(args):
+    stack = []
+    bracePairs = {}
+    for i in xrange(len(args)):
+        if args[i] == "[":
+            stack.append(i)
+        elif args[i] == "]":
+            a = stack.pop(-1)
+            bracePairs[a] = i
+            bracePairs[i] = a
+    return bracePairs
+            
+def interpret(args):
+    
+    array = [0] * 30000
+    pos = 0
+    loops = []
+    rLoops = []
+    braces = findBraces(args)
+    i = 0   
+    #print array[0:20], pos
+    while i < len(args):
+        arg = args[i]
+        if arg == ">":
+            pos += 1
+        elif arg == "<":
+            pos -= 1
+        elif arg == "+":
+            array[pos] += 1
+        elif arg == "-":
+            array[pos] -= 1
+        elif arg == ".":
+            print chr(array[pos]),
+        elif arg == ",":
+            array[pos] = ord(input("->"))
+        elif arg == "[":
+            if not array[pos]:
+                i = braces[i]
+        elif arg == "]":
+            if array[pos]:
+                i = braces[i]  
+        i += 1
+        #print array[0:20], pos, arg
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("BF", help="BF String or name of BF file.")
+    parser.add_argument("-f", "--file", help="Read from file?", action="store_true")
+    parser.add_argument("-o", "--ook", help="For the librarians?", action="store_true")
+    args = parser.parse_args()    
+    string = args.BF
+    if args.file:
+        f = open(string, "r").read().rstrip().replace(" ","").replace('\r', '').replace('\n', '')
+        if args.ook:
+            #print f
+            f = f.strip("\n").lower().replace("ook","").replace(" ","")
+            #print f
+            f = "".join(["(" + f[i] + f[i+1] + ")" for i in xrange(0, len(f) - 1, 2)])
+            #print f
+            f = f.replace("(.?)", ">").replace("(?.)","<").replace("(..)", "+").replace("(!!)", "-").replace("(!.)", ".").replace("(.!)", ",").replace("(!?)", "[").replace("(?!)", "]")
+            #f = "".join(f)
+        f = "".join([c for c in f if c in {">", "<", "+", "-", ".", ",", "[", "]"}])
+        
+    interpret(f)
+        
+    print ""    
+    #interpret("+++++[->>>+<<<]>>>.")
+if __name__ == "__main__":
+    main()
