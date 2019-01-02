0f7c769434de54f9901b05b629eeea786b4bd2c7
Theadora Williams <theadorabledeveloper@gmail.com>
2019-01-02 00:44:56 -0500

    Built a GUI for Othello. Only problem is now the game is glithching. And the GUI is a CPU drain. Probably something else. I'm tired.

---
+++ Othello/OthelloJava/src/main/java/PythonRange.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+public class PythonRange implements Iterable<Integer> {
+    private ArrayList<Integer> list = new ArrayList<Integer>();
+    private int pointer = 0;
+    public PythonRange(int end) {
+        this(0, end);
+    }
+
+    public PythonRange(int start, int end) {
+        this(start, end, 1);
+    }
+
+    public PythonRange(int start, int end, int step) {
+
+        if (start < end && step > 0) {
+            for (int i = start; i < end; i += step) {
+                list.add(i);
+            }
+        } else if (start > end && step < 0) {
+            for (int i = start; i > end; i += step) {
+                list.add(i);
+            }
+        }
+    }
+    // constructor
+    @Override
+    public Iterator<Integer> iterator() {
+        return list.iterator();
+    }
+
+
+
+}
