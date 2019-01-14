ce703370172fc2bb52fd06060d071beb9f9097a5
unknown <norbekian9@gmail.com>
2019-01-14 06:21:53 -0500

    Did some extra work the night of stuyhacks.

--- TravellingSalesman/TravellingSalesman.py
+++ TravellingSalesman/TravellingSalesman.py
@@ -21,22 +21,33 @@ def connectionsToNetlogo(connections):
 class Path:
     def __init__(self, world, startPos=None, generations=100, batchSize=100, numberPassing=10, swapChance=5):
         self.world, self.distances = world
-        self.startPos, self.generations, self.batchSize, self.numberPassing, swapChance = startPos, generations, batchSize, numberPassing, swapChance
+        self.startPos, self.generations, self.batchSize, self.numberPassing, self.swapChance = startPos, generations, batchSize, numberPassing, swapChance
         if not startPos:
             self.startPos = self.world.keys()[0]
         self.salesmen = []
         self.rejects = {" "}
-        self.places = [coord for coord in self.world if coord != startPos]
+        self.places = [coord for coord in self.world if coord != self.startPos]
         self.kirk()
         for i in xrange(generations):
             self.picard()
+        self.best = min(self.salesmen, key=lambda k : k.score)
+        #print best.score
+        #print best.route
+        a =  [self.startPos] + self.best.route + [self.startPos]
+
     def kirk(self):
         """ The first generation, to boldly go where no one has gone before. """
         for i in xrange(self.batchSize):
             self.salesmen.append(Salesman(self))
     def picard(self):
         """ The next generation. Still boldly going!"""
-        pass
+        children = []
+        self.salesmen = sorted(self.salesmen, key=lambda k : k.score)
+        for i in xrange(0, self.numberPassing, 2):
+            for child in Salesman.beFruitfulAndMultiply(self.salesmen[i], self.salesmen[i + 1], self.batchSize / self.numberPassing):
+                children.append(child)
+        self.salesmen = children
+        
 class Salesman:
     def __init__(self, path, route=None):
         self.path = path
@@ -45,7 +56,8 @@ class Salesman:
         else:
             self.route = shuffled(path.places)
         self.score = sum([path.distances[(self.route[i], self.route[i + 1])] for i in xrange(len(path.places) - 1)], getEuclideanDistance(self.route[0], path.startPos) + getEuclideanDistance(self.route[-1], path.startPos))
-    def beFruitfulAndMultiply(path, mother, father, numChildren):
+        #print self.score, " ", self.route
+    def beFruitfulAndMultiply(mother, father, numChildren):
         """ Returns the children of the the two Parents. """
         children = []
         
@@ -59,10 +71,11 @@ class Salesman:
                 if coord not in newRoute:
                     newRoute[newRoute.index(" ")] = coord
             for coord in newRoute:
-                if random.randint(100) < self.path.swapChance:
-                    r1 = random.randint(len(newRoute) - 1)
-                    r2 = random.randint(len(newRoute) - 1)                   
-            children.append(Salesman(self.path, route=newRoute))
+                if random.randint(0, 100) < mother.path.swapChance:
+                    r1 = random.randint(0, len(newRoute) - 1)
+                    r2 = random.randint(0, len(newRoute) - 1)
+                    newRoute[r1], newRoute[r2] = newRoute[r2], newRoute[r1] 
+            children.append(Salesman(mother.path, route=newRoute))
         return children
     
                     
@@ -70,10 +83,14 @@ class Salesman:
             
         
 def netlogoA(s):
-    x = makeRegularPolygon(s, 10)
+    x = makeRegularPolygon(s, 15)
     return connectionsToNetlogo(x[0])
+def netlogoB(s, g):
+    square = makeRegularPolygon(s, 15)
+    p = Path(square, generations=g)    
+    return connectionsToNetlogo([p.startPos] + p.best.route + [p.startPos])
 def main():
-    square = makeRegularPolygon(4, 10)
+    square = makeRegularPolygon(20, 10)
     p = Path(square)
 if __name__ == "__main__":
     main()
