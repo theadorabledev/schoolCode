c70f33e05693703eb279a979daccbfc2cea297a0
Theadora Williams <theadorabledeveloper@gmail.com>
2019-01-22 00:02:01 -0500

    Have done a lot of work here, more will come. Will delete database in the future, for now, hackers can come and steal my fake user's data.

---
+++ AllyNet/setupDB.py
@@ -0,0 +1,9 @@
+from app import db
+from app.models import *
+db.create_all()
+u = User(username="norbek", email="iwilliams10@stuy.edu", password_hash="1234", moderator=True, admin=True)
+u2 = User(username="bob", email="bob@bob.edu", password_hash="12345", moderator=False, admin=False)
+
+db.session.add(u)
+db.session.add(u2)
+db.session.commit()
