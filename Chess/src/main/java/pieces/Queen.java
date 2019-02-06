06c8ac896c0149e00f458daf92d033fe0678267b
ianStuy <norbekian9@gmail.com>
2019-02-06 09:27:15 -0500

    Got rid of queen's inner pieces.

--- Chess/src/main/java/pieces/Queen.java
+++ Chess/src/main/java/pieces/Queen.java
@@ -5,25 +5,41 @@ public class Queen extends Piece{
 	private Rook rook;
 	public Queen(Coordinate c, int s, Game g){
 		super(c, s, symbol, g);
-		bishop = new Bishop(c, s, g);
-		rook = new Rook(c, s, g);
+		//bishop = new Bishop(c, s, g);
+		//rook = new Rook(c, s, g);
 	}
 	public boolean isValidPlay(Coordinate c, boolean theoretical){
-		if(!super.isValidPlaySuper(c)) return false;
-		return (bishop.isValidPlay(c) || rook.isValidPlay(c));
+	    if(!super.isValidPlaySuper(c))  return false;
+	    if(!(((c.x == pos.x) ^ (c.y == pos.y)) || (Math.abs(c.x - pos.x) == Math.abs(c.y - pos.y)))) return false;
+	    if(Math.abs(c.x - pos.x) != Math.abs(c.y - pos.y)){
+		int xDir = (c.x - pos.x) / Math.abs(c.x - pos.x);
+		int yDir = (c.y - pos.y) / Math.abs(c.y - pos.y);
+		for(int i : new PythonRange(1, Math.abs(c.x - pos.x))){
+		    if(game.pieceAt(pos.x + (i * xDir), pos.y + (i * yDir)) != null) return false;
+		}
+	    }else{
+		int xDir = (c.x - pos.x == 0) ? 0 : (c.x - pos.x) / Math.abs(c.x - pos.x);
+		int yDir = (c.y - pos.y == 0) ? 0 : (c.y - pos.y) / Math.abs(c.y - pos.y);
+		for(int i : new PythonRange(1, Math.max(Math.abs(c.x - pos.x), Math.abs(c.y - pos.y)))){
+		    if(game.pieceAt(pos.x + (i * xDir), pos.y + (i * yDir)) != null) return false;
+		}
+	    }
+	    return (theoretical || tryMove(c));
+		//return (bishop.isValidPlay(c) || rook.isValidPlay(c));
 	}
 	public void move(Coordinate c){
 		super.move(c);
-		bishop.move(c);
-		rook.move(c);
+		//bishop.move(c);
+		//rook.move(c);
 	}
 	public void betray(int s){
 		side = s;
-		bishop.side = s;
-		rook.side = s;
+		//bishop.side = s;
+		//rook.side = s;
 	}
 	public void setKing(King k){
-		king = bishop.king =rook.king = k;
+	    //king = bishop.king =rook.king = k;
+	    king = k;
 	}
 
 }
