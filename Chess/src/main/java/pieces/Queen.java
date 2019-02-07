2e07f8a21c0a460b8472db391a3cd9a45c3da14e
unknown <norbekian9@gmail.com>
2019-02-07 06:45:51 -0500

    Seem to have made real progress. Need to fix piece taking during movement.

--- Chess/src/main/java/pieces/Queen.java
+++ Chess/src/main/java/pieces/Queen.java
@@ -9,22 +9,21 @@ public class Queen extends Piece{
 		//rook = new Rook(c, s, g);
 	}
 	public boolean isValidPlay(Coordinate c, boolean theoretical){
-	    if(!super.isValidPlaySuper(c))  return false;
-	    if(!(((c.x == pos.x) ^ (c.y == pos.y)) || (Math.abs(c.x - pos.x) == Math.abs(c.y - pos.y)))) return false;
-	    if(Math.abs(c.x - pos.x) != Math.abs(c.y - pos.y)){
-		int xDir = (c.x - pos.x) / Math.abs(c.x - pos.x);
-		int yDir = (c.y - pos.y) / Math.abs(c.y - pos.y);
-		for(int i : new PythonRange(1, Math.abs(c.x - pos.x))){
-		    if(game.pieceAt(pos.x + (i * xDir), pos.y + (i * yDir)) != null) return false;
+		if (!super.isValidPlaySuper(c)) return false;
+		if (!(((c.x == pos.x) ^ (c.y == pos.y)) || (Math.abs(c.x - pos.x) == Math.abs(c.y - pos.y)))) return false;
+		int xDir;
+		int yDir;
+		if (Math.abs(c.x - pos.x) == Math.abs(c.y - pos.y)) {//Bishop
+			xDir = (c.x - pos.x) / Math.abs(c.x - pos.x);
+			yDir = (c.y - pos.y) / Math.abs(c.y - pos.y);
+		} else {//Rook
+			xDir = (c.x - pos.x == 0) ? 0 : (c.x - pos.x) / Math.abs(c.x - pos.x);
+			yDir = (c.y - pos.y == 0) ? 0 : (c.y - pos.y) / Math.abs(c.y - pos.y);
 		}
-	    }else{
-		int xDir = (c.x - pos.x == 0) ? 0 : (c.x - pos.x) / Math.abs(c.x - pos.x);
-		int yDir = (c.y - pos.y == 0) ? 0 : (c.y - pos.y) / Math.abs(c.y - pos.y);
-		for(int i : new PythonRange(1, Math.max(Math.abs(c.x - pos.x), Math.abs(c.y - pos.y)))){
-		    if(game.pieceAt(pos.x + (i * xDir), pos.y + (i * yDir)) != null) return false;
+		for (int i : new PythonRange(1, Math.max(Math.abs(c.x - pos.x), Math.abs(c.y - pos.y)))) {
+			if (game.pieceAt(pos.x + (i * xDir), pos.y + (i * yDir)) != null) return false;
 		}
-	    }
-	    return (theoretical || tryMove(c));
+		return (theoretical || tryMove(c));
 		//return (bishop.isValidPlay(c) || rook.isValidPlay(c));
 	}
 	public void move(Coordinate c){
