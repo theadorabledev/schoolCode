a2a9e3b893f844ca73e5fc9bdfd7c5f37295ab5f
unknown <norbekian9@gmail.com>
2019-02-14 22:49:14 -0500

    Added command line usage. Need to fix scrambled translate bug in which the program chooses a chosen button. Will also probably add account managering features.

--- memrise/mem.py
+++ memrise/mem.py
@@ -3,13 +3,20 @@ import re
 from splinter import Browser
 from selenium.webdriver.common.keys import Keys
 import selenium
+import argparse
 import time
 import json
 import getpass
+def isPoints(browser):
+    """ Returns whether the page contains the earned points. """
+    pts = browser.find_by_css('span[class="pts"]')
+    if pts:
+        return pts[3].text
+    return None
 def isMultipleChoice(browser):
     """ Returns whether or not you are being given a multiple choice problem. """
     return browser.is_element_present_by_css('span[class="val  "]')
-def answerMultipleChoice(browser, data):
+def answerMultipleChoice(browser, data, question):
     """ Solves the multiple choice problem. """
     for val in browser.find_by_css('span[class="val  "]'):#Checks possible answers
         if ( question in data and val.text == data[question]) or (val.text in data and data[val.text] == question): # Hopefully deals with definition repetion creating json problems
@@ -26,6 +33,7 @@ def typeTranslation(browser, answer):
     browser.find_by_css('input[class="roundy shiny-box typing-type-here  "]').first.fill(answer + "\n")
 def scrambleTranslate(browser, answer):
     """ Answers the scrambled translate problems. """
+    #Need to deal with word repitition.
     sentence = re.findall(r"[\w']+|[.,!?;]", answer, re.UNICODE)
     for word in sentence:
         for wordButton in browser.find_by_css('div[class="word btn"]'):
@@ -51,18 +59,27 @@ def loadBrowser(url):
 #browser = Browser('chrome', **executable_path)
         browser = Browser(**executable_path)
     browser.visit(url)
+    cookies = browser.find_by_css('a[class="cc-btn cc-allow"]') # Accept cookies button
+    if cookies:
+        cookies.first.click()    
     browser.fill("username", data["username"])
     browser.fill("password", data["password"])
+    #<a aria-label="allow cookies" role="button" tabindex="0" class="cc-btn cc-allow">I agree</a>
+    
     return browser
-def learn():
+def learn(lessonNumber=""):
     """ Automates the learning on memrise. """
     data = loadData()
     print "Translations loaded."
-    browser = loadBrowser('https://www.memrise.com/course/1992222/descubre-2-5/garden/learn/')
+    url = 'https://www.memrise.com/course/1992222/descubre-2-5/garden/learn/'
+    if lessonNumber:
+        url = 'https://www.memrise.com/course/1992222/descubre-2-5/' + str(lessonNumber) + '/garden/learn/'
+    browser = loadBrowser(url)
     time.sleep(2)
     lastQuestion = "?"
     looping = True
     loops = 0
+    #raw_input(1)
     while looping:
         question = browser.find_by_css('div[class="qquestion qtext  "]') #Read question 
         loops += 1
@@ -71,20 +88,22 @@ def learn():
             loops = 0
             lastQuestion = question
             if isMultipleChoice(browser):
-                answerMultipleChoice(browser, data)   
+                answerMultipleChoice(browser, data, question)   
             elif isTyping(browser):
                 typeTranslation(browser, data[question])
             else:
                 scrambleTranslate(browser, data[question])
             time.sleep(.5)
-        
-        elif loops > 6:
-            print browser.find_by_css('span[class="pts"]')[3].text
-            looping = False
-            break  
-        else:#Definition
+            
+        else:#Definition or endpage
             if browser.is_element_present_by_css('span[class="next-icon"]'):
                 browser.find_by_css('span[class="next-icon"]').first.click()
+            else:
+                pts = isPoints(browser)
+                if pts:
+                    print pts
+                    looping = False
+                    break            
             time.sleep(.5)
     browser.quit()
 def review():
@@ -98,20 +117,22 @@ def review():
     loops = 0
     while looping:
         question = browser.find_by_css('div[class="qquestion qtext  "]') #Read question
+        loops += 1
         if question and question.first.text != lastQuestion: # Check if still on old question
             question = question.first.text
             loops = 0
             lastQuestion = question
             if isMultipleChoice(browser):
-                answerMultipleChoice(browser, data)                               
+                answerMultipleChoice(browser, data, question)                               
             elif isTyping(browser):
                 typeTranslation(browser, data[question])
             time.sleep(.5)
-        loops += 1
-        if loops > 6:
-            print browser.find_by_css('span[class="pts"]')[3].text
-            looping = False
-            break         
+        else:
+            pts = isPoints(browser)
+            if pts:
+                print pts
+                looping = False
+                break         
     browser.quit()   
 def speed_review():
     """ Automates the speed review on memrise. """
@@ -124,22 +145,25 @@ def speed_review():
     loops = 0
     while looping:
         question = browser.find_by_css('div[class="qquestion qtext  "]').first.text #Read question
+        loops += 1
         if question != lastQuestion: # Check if still on old question
             loops = 0
-            answerMultipleChoice(browser, data)
+            answerMultipleChoice(browser, data, question)
             lastQuestion = question      
-        loops += 1
-        time.sleep(.5)
-        if loops > 6:
+            #time.sleep(.5)
+            time.sleep(.1)
+        elif loops > 6:
             try:
                 active_web_element = browser.driver.switch_to_active_element()  
                 active_web_element.send_keys(Keys.PAGE_DOWN)
                 active_web_element.send_keys(Keys.ENTER) # Presses enter to continue to next page
             except:
-                pass
-            print browser.find_by_css('span[class="pts"]')[3].text
-            looping = False
-            break        
+                pass            
+            pts = isPoints(browser)
+            if pts:
+                print pts
+                looping = False
+                break       
     browser.quit()   
 def loadUser():
     """ Returns login information if saved, if not, asks for it and saves it. """
@@ -160,8 +184,29 @@ def loadData():
     with open('data.json', 'r') as fp:
         return json.load(fp)    
 def main():
-    learn()
-    #review()
-    #speed_review()
+    parser = argparse.ArgumentParser(epilog="""
+    Times and Unit are optional.
+    Unit must be used with the learn command.
+    You can not combine learn/review/speed.
+    """)
+    parser.add_argument('-l', "--learn", help="Learn.", action="store_true")
+    parser.add_argument('-r', "--review", help="Review.", action="store_true")
+    parser.add_argument('-s', "--speed", help="Speed Review.", action="store_true")
+    parser.add_argument('-t', "--times", help="Repeats the action times times.", type=int, default=1)
+    parser.add_argument('-u', "--unit", help="Perform the action for this unit.", type=int)
+    args = parser.parse_args()
+    if not ((args.learn != args.review) != args.speed):
+        parser.print_help()
+    else:
+        for i in xrange(args.times):
+            if args.learn:
+                if args.unit:
+                    learn(lessonNumber=str(args.unit))
+                else:
+                    learn()
+            elif args.review:
+                review()
+            elif args.speed:
+                speed_review()
 if __name__ == "__main__":
     main()
