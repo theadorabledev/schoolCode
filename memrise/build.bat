69176cdff2a025e49810fbd42c30d34c0c4d61a7
Theadora Williams <theadorabledeveloper@gmail.com>
2019-02-19 17:31:10 -0500

    Extra work on distribution.

--- memrise/build.bat
+++ memrise/build.bat
@@ -3,16 +3,15 @@ set mac=%1
 python generate.py %mac% memrise_helper.py
 set PROJECT_NAME=temp
 set PYTHON_DIR=C:\python27
-mkdir build
 python -m cython --embed -o memrise_helper.c memrise_helper.py
 gcc -c -IC:\Python27\include -o memrise_helper.o memrise_helper.c
 gcc -shared -LC:\Python27\libs -o memrise_helper.pyd memrise_helper.o -lpython27
 ::pyinstaller -y main.spec
-pyinstaller -y --add-binary "chromedriver.exe;." main.py
 pyinstaller -y --onefile --name memrise-helper --add-binary "chromedriver.exe;." main.py
 del *.o
-del *.c
-del *.spec
+del memrise_helper.*
+rmdir /S /Q build
+::del *.spec
 
 ::gcc -Os -I %PYTHON_DIR%\include -o memrise_helper.exe temp.c -lpython27 -lm -L %PYTHON_DIR%\libs
 ::del temp.c
