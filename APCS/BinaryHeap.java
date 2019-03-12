726c3a0930a4afd98cd5c68370187c969cac9f9a
ianStuy <norbekian9@gmail.com>
2019-03-12 09:28:10 -0400

    Finished heap sort, starting some advaned project euler.

--- APCS/BinaryHeap.java
+++ APCS/BinaryHeap.java
@@ -18,14 +18,17 @@ public class BinaryHeap{
 	if (data[i] > data[(i-1)/2]) return false;
 	return isHeapHelper(data,i+1);
     }
-    public static void heapify(int [] data, int i){
+    public static void heapify(int[] data, int i){
+	heapify(data, i, data.length);
+    }
+    public static void heapify(int [] data, int i, int upper){
 	int left = 2 * i + 1;
 	int right = left + 1;
-	if(left >= data.length || i < 0) return;
-	int maxChildPos = (right < data.length && data[right] > data[left])? right : left;
+	if(left >= upper || i < 0) return;
+	int maxChildPos = (right < upper && data[right] > data[left])? right : left;
 	if(data[i] < data[maxChildPos]){
 	    swap(data, i, maxChildPos);
-	    heapify(data, maxChildPos);
+	    heapify(data, maxChildPos, upper);
 	}
 
 	//heapify(data, i - 1);
@@ -35,11 +38,18 @@ public class BinaryHeap{
 	    heapify(data, i);
 	}
     }
+    public static void heapSort(int[] data){
+	buildHeap(data);
+	for(int i = data.length - 1; i > 0; i--){
+	    swap(data, 0, i);
+	    heapify(data, 0, i);
+	}
+    }
     public static void main(String [] args){
-	//int [] bheap = {1, 14, 7, 8, 3, 19};
-	int [] bheap = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
-	    print(bheap);
-	    buildHeap(bheap);
-	    print(bheap);
+	
+	int [] bheap = {17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+	print(bheap);
+	heapSort(bheap);
+	print(bheap);
     }
 }
