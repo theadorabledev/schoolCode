36b85da5148d9b4ccbd8a782cd6ae39352e57af6
Theadora Williams <theadorabledeveloper@gmail.com>
2019-03-19 09:28:38 -0400

    I ave a cunnin plan.

--- euler/Problem_603.java
+++ euler/Problem_603.java
@@ -1,9 +1,22 @@
 public class Problem_603{
+
+    public static void print(Object ... list){
+	for(Object o : list){
+	    System.out.print(o + " ");
+	}
+	System.out.println();
+    }
 	public static int S(String n, int l){
+	    int sTest = 0;
+	   
+
+	    
 		int N = n.length();
 		int NN = N * l;
 		int s = 0;
 		int t = (((int) Math.pow(10, NN) - 1) / 9);
+		for()
+
 		for(int i = 0; i < NN; i++){
 			s += Character.getNumericValue(n.charAt(i % N)) * (i + 1) * t;
 			t /= 10;
@@ -26,9 +39,13 @@ public class Problem_603{
 		return p;
 	}
 	public static void main(String [] args){
-		String p = P((int) Math.pow(10,6));
-		int sum = S(p, (int) Math.pow(10, 12));
-		int mod = (int) Math.pow(10, 9) + 7;
-		System.out.println(sum % mod);
+	    System.out.println(S("123123", 1)+ " "+ S("123",2));
+	    System.out.println(P(7));
+	    System.out.println(S(P(7),3 ) + " " + S(P(7)+P(7)+P(7), 1));
+	    print(S(P((int) Math.pow(10, 3)), (int) Math.pow(10,12)) % 10007);
+	    //String p = P((int) Math.pow(10,6));
+	    //int sum = S(p, (int) Math.pow(10, 12));
+	    //int mod = (int) Math.pow(10, 9) + 7;
+	    //System.out.println(sum % mod);
 	}
 }
