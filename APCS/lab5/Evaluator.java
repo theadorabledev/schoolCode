738cf1422f9b299c004a22cf207b1cf3e5bf2ea8
ianStuy <norbekian9@gmail.com>
2019-03-28 09:28:00 -0400

    Started work on lab 5

---
+++ APCS/lab5/Evaluator.java
@@ -0,0 +1,81 @@
+import java.util.*;
+public class Evaluator{
+
+  
+    // precondition: exp is a String expression that is fully parenthesized (a set
+    //               of parentheses for each operator).
+    // postcodition: returns a String [] of the tokens of exp.
+    //               ex. getTokens("(5-2.2)") -> ["(","5","-","2.2",")"]
+    public static String[] getTokens(String exp){
+        exp = exp.replaceAll(" ", "");
+        String[] tokens = exp.split("(?<=[^\\.a-zA-Z\\d])|(?=[^\\.a-zA-Z\\d])");
+        return tokens;
+    }
+    
+
+    // precondition: receives an array of the tokens of an infix expression.
+    //               tokens must be one of the following:
+    //               (),+-*/%,sqrt
+    // postcondition: evalutes the expression represented in expTokens
+    //             ex. evaluate(["(","5","-","2.2",")"]) returns 2.8
+    public static Double evaluate(String[] expTokens){
+	System.out.println(Arrays.asList(expTokens));
+	Stack<String> stack = new Stack<String>();
+	for(int i = 0; i < expTokens.length; i++){
+	    if(expTokens[i].equals(")")){
+		    String[] arr = new String[3];
+		    int pos = 2;
+		    while(!stack.peek().equals("(")){
+			System.out.println(i + " "+ pos);
+			arr[pos] = stack.pop();
+			pos --;
+		    }
+		    stack.pop();
+		    stack.push(minEval(arr));
+	    }else{
+		stack.push(expTokens[i]);
+	    }
+	}
+        return new Double(stack.pop());
+    }
+	public static String minEval(String[] arr){
+	    System.out.println(Arrays.asList(arr));
+	    double d;
+	    if(arr[1].equals("+")){
+		d = Double.valueOf(arr[0]) + Double.valueOf(arr[2]);
+	    }else if(arr[1].equals("-")){
+		d = Double.valueOf(arr[0]) - Double.valueOf(arr[2]);
+	    }
+	    else if(arr[1].equals("*")){
+		d = Double.valueOf(arr[0]) * Double.valueOf(arr[2]);
+	    }
+	    else if(arr[1].equals("/")){
+		d = Double.valueOf(arr[0]) / Double.valueOf(arr[2]);
+	    }
+	    else if(arr[1].equals("%")){
+		d = Double.valueOf(arr[0]) * Double.valueOf(arr[2]);
+	    }else{
+		d = Math.sqrt(Double.valueOf(arr[2]));
+	    }
+	    System.out.println(d);
+	    return d + "";
+	}
+       
+    public static void main(String [] args){
+        
+        String[] expressions = {"(2+3)","(4 + (3 - 2))", "((5 *(10 + (32 - 8))/ 2.0))",
+                                "( sqrt  (16 * 16) )", "(( 25 % 3 ) + 9)"};
+        Double[] expectedResults = {5.0,5.0,85.0,16.0,10.0};
+
+        for (int i = 0; i < expressions.length; i++){
+            String[] tokens = getTokens(expressions[i]);
+            Double  ansObserved = evaluate(tokens);
+            Double  ansExpected = expectedResults[i];
+            System.out.println(expressions[i] + " = " + evaluate(tokens));
+            System.out.println("observed: " + ansObserved + " expected: " + ansExpected);
+            System.out.println("correct? " + (ansObserved.equals( ansExpected)));
+        }
+    }
+    
+}
+
