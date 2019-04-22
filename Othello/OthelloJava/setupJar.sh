6e68e42021af3c3c1c06e18c78e280c4f47d05bb
Theadora Williams <theadorabledeveloper@gmail.com>
2019-04-22 17:01:59 -0400

    Moved some stuff to its own repo

--- Othello/OthelloJava/setupJar.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-javac -d target/bin src/main/java/*.java
--- jProfiler.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#Gets the pid of the running java proccess
-jpid=`(jcmd -l | grep -v "jcmd" | grep -oP "(.*) ")`
-jcmd $jpid help
-
