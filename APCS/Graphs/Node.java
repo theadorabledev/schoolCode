a44387a2546cdb044220aac0a59bc059830b9b56
Theadora Williams <theadorabledeveloper@gmail.com>
2019-04-30 20:02:44 -0400

    Finished djikstras and hw

--- APCS/Graphs/Node.java
+++ APCS/Graphs/Node.java
@@ -2,42 +2,48 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.*;
 public class Node<E>{
-    private final E VALUE;
-    //private ArrayList<Node<E>> connections = new ArrayList<Node<E>>();
-    private HashMap<Node<E>, Integer> connections = new HashMap<Node<E>, Integer>();
-    public Node(E value){
-	VALUE = value;
-    }
-    public E getValue(){
-	return VALUE;
-    }
-    public Set<Node<E>> getConnections(){
-	return connections.keySet();
-    }
-    public void connect(Node<E> n){
-	//Adds path from this to n
-	//connections.add(n);
-	connect(n, 1);
-    }
-    public void connect(Node<E> n, int weight){
-	connections.put(n, weight);
-    }
-    @Override
-    public String toString(){
-	return VALUE.toString();
-    }
-    @Override
-    public boolean equals(Object o) {
-	if (this == o) return true;
-	if (o == null || getClass() != o.getClass()) return false;
+	private final E VALUE;
+	//private ArrayList<Node<E>> connections = new ArrayList<Node<E>>();
+	private HashMap<Node<E>, Integer> connections = new HashMap<Node<E>, Integer>();
+	public Node(E value){
+		VALUE = value;
+	}
+	public E getValue(){
+		return VALUE;
+	}
+	public Set<Node<E>> getConnections(){
+		return connections.keySet();
+	}
+	public HashMap<Node<E>, Integer> connections(){
+			return connections;
+	}
+	public void connect(Node<E> n){
+		//Adds path from this to n
+		//connections.add(n);
+		connect(n, 1);
+	}
+	public void connect(Node<E> n, int weight){
+		connections.put(n, weight);
+	}
+	public int distanceTo(Node<E> n){
+		return connections.get(n);
+	}
+	@Override
+	public String toString(){
+		return VALUE.toString();
+	}
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		
+		Node<E> n = (Node<E>) o;
+		return VALUE.equals(n.getValue());
+	}
 	
-	Node<E> n = (Node<E>) o;
-	return VALUE.equals(n.getValue());
-    }
-    
-    @Override
-    public int hashCode() {
-	return VALUE.hashCode();
-	    
-    }
+	@Override
+	public int hashCode() {
+		return VALUE.hashCode();
+		
+	}
 }
