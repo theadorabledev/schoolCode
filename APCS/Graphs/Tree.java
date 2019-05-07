4dcbc86c7e1bfd58ebd4b4e20141eb50b6f428e9
ianStuy <norbekian9@gmail.com>
2019-05-07 09:28:12 -0400

    More work on the fib heap

--- APCS/Graphs/Tree.java
+++ APCS/Graphs/Tree.java
@@ -1,23 +1,37 @@
+import java.util.*;
 public class Tree<E>{
     private E root;
     private ArrayList<Tree<E>> children;
+    private int treeRank = 1;
     public Tree(E root){
-	this.root = r;
+	this.root = root;
     }
     public void addChild(E child){
-	children.add(new Tree<E>(child));
+	addChild(new Tree<E>(child));
     }
     public void addChild(Tree<E> child){
 	children.add(child);
+	treeRank += child.treeRank();
     }
     public int rank(){
 	return children.size();
     }
+    public int treeRank(){
+	return treeRank;
+    }
     public E root(){
 	return root;
     }
-    public ArrayList(<Tree<E>> children){
+    public ArrayList<Tree<E>> children(){
 	return children;
+    }
+    public String toString(){
+	String s = root + " : [";
+	for(Tree<E> child : children){
+	    s += child + ", ";
+	}
+	s += "]";
+	return s.replace(", ]", " ]");
     }
     
 }
