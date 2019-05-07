636242d0819f5c32782e511ae9ede4763c624f3e
Theadora Williams <theadorabledeveloper@gmail.com>
2019-05-07 09:28:12 -0400

    More work on the fib heap

--- APCS/Graphs/FibonacciHeap.java
+++ APCS/Graphs/FibonacciHeap.java
@@ -1,21 +1,35 @@
+import java.util.*;
 public class FibonacciHeap<E>{
     private DNode<Tree<E>> head;
     private DNode<Tree<E>> min;
+    private Comparator<E> comp;
     private int size = 0;
-    public FibonacciHeap(E val){
+    public FibonacciHeap(E val, Comparator<E> comp){
 	head = new DNode<Tree<E>>(new Tree(val), null, null);
 	head.setNext(head);
 	head.setPrevious(head);
 	min = head;
+	if(comp != null){
+	    this.comp = comp;
+	}else{
+	    class MyComparator implements Comparator<E> {
+		public int compare(E a, E b) {
+		    return a.compareTo(b);
+		}
+	    };
+	    this.comp = new MyComparator();
+	}
+	
+	
     }
     public int size(){
 	return size;
     }
     public E min(){
-	return min.getValue.root();
+	return min.getValue().root();
     }
     public void insert(Tree<E> t){
-	Dnode<Tree<E>> node = new Dnode<Tree<E>>(t, head, head.getNext());
+	DNode<Tree<E>> node = new DNode<Tree<E>>(t, head, head.getNext());
 	head.getNext().setPrevious(node);
 	head.setNext(node);
 	if(t.root() < min()){
@@ -26,13 +40,35 @@ public class FibonacciHeap<E>{
 	Tree<E> t = new Tree<E>(val);
 	insert(t);
     }
+    public void merge(DNode<Tree<E>> a, DNode<Tree<E>> b){
+	if(a.getValue().root() > b.getValue().root()){
+	    b.addChild(remove(a).getValue());
+	}
+	else{
+	    a.addChild(remove(b).getValue());
+	}
+    }
+    public DNode<Tree<E>> remove(DNode<Tree<E>> node){
+	DNode<Tree<E>> next = node.getNext();
+	DNode<Tree<E>> last = node.getPrevious();
+	next.setPrevious(last);
+	last.setNext(next);
+    }
     public void DeleteMin(){
 	for(Tree<E> child : min.getValue().children()){
 	    insert(child);
 	}
-	DNode<E> next = min.getNext();
-	DNode<E> last = min.getPrevious();
-	next.setPrevious(last);
-	last.setNext(next);
+	remove(min);
+    }
+    public void print(){
+	DNode<Tree<E>> node = head.getNext();
+	System.out.println(head);
+	while(node != head){
+	    System.out.println(node);
+	    node = node.getNext();
+	}
+    }
+    public static void main(String[] args){
+	FibonacciHeap<Integer> fib = new FibonacciHeap<Integer>();
     }
 }
