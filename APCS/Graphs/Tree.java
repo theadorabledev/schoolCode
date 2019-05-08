262e01a4ecfc0876b78ff3a1f1a42f7a351662ea
unknown <norbekian9@gmail.com>
2019-05-07 21:12:31 -0400

    Reorganized folder, did the HW, and finished delete-min for the Fibonacci heap!

--- APCS/Graphs/Tree.java
+++ APCS/Graphs/Tree.java
@@ -1,37 +1,37 @@
 import java.util.*;
 public class Tree<E>{
-    private E root;
-    private ArrayList<Tree<E>> children;
-    private int treeRank = 1;
-    public Tree(E root){
-	this.root = root;
-    }
-    public void addChild(E child){
-	addChild(new Tree<E>(child));
-    }
-    public void addChild(Tree<E> child){
-	children.add(child);
-	treeRank += child.treeRank();
-    }
-    public int rank(){
-	return children.size();
-    }
-    public int treeRank(){
-	return treeRank;
-    }
-    public E root(){
-	return root;
-    }
-    public ArrayList<Tree<E>> children(){
-	return children;
-    }
-    public String toString(){
-	String s = root + " : [";
-	for(Tree<E> child : children){
-	    s += child + ", ";
+	private E root;
+	private ArrayList<Tree<E>> children = new ArrayList<Tree<E>>();
+	private int treeRank = 1;
+	public Tree(E root){
+		this.root = root;
 	}
-	s += "]";
-	return s.replace(", ]", " ]");
-    }
-    
+	public void addChild(E child){
+		addChild(new Tree<E>(child));
+	}
+	public void addChild(Tree<E> child){
+		children.add(child);
+		treeRank += child.treeRank();
+	}
+	public int rank(){
+		return children.size();
+	}
+	public int treeRank(){
+		return treeRank;
+	}
+	public E root(){
+		return root;
+	}
+	public ArrayList<Tree<E>> children(){
+		return children;
+	}
+	public String toString(){
+		String s = root + " : [ ";
+		for(Tree<E> child : children){
+			s += child + ", ";
+		}
+		s += "]";
+		return s.replace(", ]", " ]").replace(" : [ ]", "");
+	}
+	
 }
