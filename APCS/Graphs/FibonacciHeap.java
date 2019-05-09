5b8c3f179c2324010d3f92a09844bbb413edc817
Theadora Williams <theadorabledeveloper@gmail.com>
2019-05-08 20:40:06 -0400

    Seem to have finished the Fibonacci Heap!

--- APCS/Graphs/FibonacciHeap.java
+++ APCS/Graphs/FibonacciHeap.java
@@ -1,179 +1,201 @@
 import java.util.*;
 public class FibonacciHeap<E>{
-    private DNode<Tree<E>> head;
-    private DNode<Tree<E>> min;
-    private Comparator<E> comp;
-    private int size = 0;
-    public FibonacciHeap(){
-	this(null);
-    }
-    public FibonacciHeap(Comparator<E> comp){
-	head = Tree<E>(null, null, null);
-	head.setNext(head);
-	head.setPrevious(head);
-	min = head;
-	if(comp != null){
-	    this.comp = comp;
-	}else{
-	    class MyComparator implements Comparator<E> {
-		public int compare(E a, E b) {
-		    return ((Comparable) a).compareTo(b);
+	private Tree<E> head;
+	private Tree<E> min;
+	private Comparator<E> comp;
+	private int size = 0;
+	private HashMap<E, Tree<E>> locations = new HashMap<E, Tree<E>>();
+	public FibonacciHeap(){
+		this(null);
+	}
+	public FibonacciHeap(Comparator<E> comp){
+		head = new Tree<E>(null, null, null);
+		head.setNext(head);
+		head.setPrevious(head);
+		min = head;
+		if(comp != null){
+			this.comp = comp;
+		}else{
+			class MyComparator implements Comparator<E> {
+				public int compare(E a, E b) {
+					return ((Comparable) a).compareTo(b);
+				}
+			};
+			this.comp = new MyComparator();
 		}
-	    };
-	    this.comp = new MyComparator();
 	}
-    }
-    public int size(){
-	return size;
-    }
-    public E min(){
-	return min.getValue().root();
-    }
-    public void insert(Tree<E> t){
-	Tree<E> node = new Tree<E>(t, head, head.getNext());
-	head.getNext().setPrevious(node);
-	head.setNext(node);
-	if(comp.compare(t.root(), min()) < 0){
-	    min  = node;
+	public int size(){
+		return size;
 	}
-	size++;
-    }
-    public void insert(E val){
-	if(size == 0){
-	    head.setRoot(val);
-	    size++;
-	}else{
-	    Tree<E> t = new Tree<E>(val);
-	    insert(t);
+	public E min(){
+		return min.root();
 	}
-    }
-    public void add(E val){
-	insert(val);
-    }
-    public DNode<Tree<E>> merge(Tree<E> a, Tree<E> b){ //returns the new parent
-	if(compareNodes(a, b) > 0){
-	    b.addChild(remove(a));
-	    return b;
+	public void insert(Tree<E> node){
+		node.setNext(head.getNext());
+		node.setPrevious(head);
+		head.getNext().setPrevious(node);
+		head.setNext(node);
+		if(comp.compare(node.root(), min()) < 0){
+			min  = node;
+		}
+		size++;
 	}
-	else{
-	    a.addChild(remove(b));
-	    return a;
+	public void insert(E val){
+		if(size == 0){
+			head.setRoot(val);
+			locations.put(val, head);
+			size++;
+		}else{
+			Tree<E> t = new Tree<E>(val);
+			locations.put(val, t);
+			insert(t);
+		}
 	}
-    }
-    public DNode<Tree<E>> remove(DNode<Tree<E>> node){
-	DNode<Tree<E>> next = node.getNext();
-	DNode<Tree<E>> last = node.getPrevious();
-	next.setPrevious(last);
-	last.setNext(next);
-	if(node == head) head = node.getPrevious();
-	return node;
-    }
-    public E deleteMin(){
-	//Move min's children to list
-	for(Tree<E> child : min.getValue().children()){
-	    insert(child);
+	public void add(E val){
+		insert(val);
+		size++;
 	}
-	//Remove min
-	DNode<Tree<E>> m = min;
-	remove(min);
-	///Update min
-	DNode<Tree<E>> node = head.getNext();
-	if(compareNodes(head, min) < 0) min = head;
-	while(node != head){
-	    if(compareNodes(node, min) < 0) min = node;
-	    node = node.getNext();
+	public Tree<E> merge(Tree<E> a, Tree<E> b){ //returns the new parent
+		if(compareNodes(a, b) > 0){
+			b.addChild(remove(a));
+			return b;
+		}
+		else{
+			a.addChild(remove(b));
+			return a;
+		}
 	}
-	//Consolidate trees
-	HashMap<Integer, DNode<Tree<E>>> ranks = new HashMap<Integer, DNode<Tree<E>>>();
-	node = head.getNext();
-	ranks.put(head.getValue().treeRank(), head);
-	while(node != head){
-	    DNode<Tree<E>> nextNode = node.getNext();
-	    DNode<Tree<E>> compNode = ranks.get(node.getValue().treeRank());
-	    System.out.println("\n\n\n\n");
-	    System.out.println("head, node, next : " + head + " " + node + " " + nextNode);
-	    System.out.println(ranks);
-	    print();
+	public Tree<E> remove(Tree<E> node){
+		Tree<E> next = node.getNext();
+		Tree<E> last = node.getPrevious();
+		next.setPrevious(last);
+		last.setNext(next);
+		if(node == head) head = node.getPrevious();
+		return node;
+	}
+	public E deleteMin(){
+		//Move min's children to list
+		for(Tree<E> child : min.children()){
+			insert(child);
+		}
+		//Remove min
+		Tree<E> m = min;
+		remove(min);
+		///Update min
+		Tree<E> node = head.getNext();
+		if(compareNodes(head, min) < 0) min = head;
+		while(node != head){
+			if(compareNodes(node, min) < 0) min = node;
+			node = node.getNext();
+		}
+		//Consolidate trees
+		HashMap<Integer, Tree<E>> ranks = new HashMap<Integer, Tree<E>>();
+		node = head.getNext();
+		ranks.put(head.treeRank(), head);
+		while(node != head){
+			Tree<E> nextNode = node.getNext();
+			Tree<E> compNode = ranks.get(node.treeRank());
+			System.out.println("\n\n\n\n");
+			System.out.println("head, node, next : " + head + " " + node + " " + nextNode);
+			System.out.println(ranks);
+			print();
 			
 			
-	    if(compNode != null && compNode != node){
-		ranks.remove(compNode.getValue().treeRank());
-		DNode<Tree<E>> newNode = merge(compNode, node);
-		if(newNode == compNode){
-		    remove(node);
-		}else{
-		    remove(compNode);
-		}
-		if(compNode == head || node == head) head = newNode;
-		//node = nextNode;//newNode;
-		if(ranks.get(newNode.getValue().treeRank()) == null){
-		    ranks.put(newNode.getValue().treeRank(),newNode);
-		    node = nextNode;
-		}else{
-		    //node = newNode;
-		}
-	    }else{
-		ranks.put(node.getValue().treeRank(), node);
-		node = nextNode;
-	    }
-	    print();
-	    System.out.println("head, node, next : " + head + " " + node + " " + nextNode);
+			if(compNode != null && compNode != node){
+				ranks.remove(compNode.treeRank());
+				Tree<E> newNode = merge(compNode, node);
+				if(newNode == compNode){
+					remove(node);
+				}else{
+					remove(compNode);
+				}
+				if(compNode == head || node == head) head = newNode;
+				//node = nextNode;//newNode;
+				if(ranks.get(newNode.treeRank()) == null){
+					ranks.put(newNode.treeRank(),newNode);
+					node = nextNode;
+				}else{
+					//node = newNode;
+				}
+			}else{
+				ranks.put(node.treeRank(), node);
+				node = nextNode;
+			}
+			print();
+			System.out.println("head, node, next : " + head + " " + node + " " + nextNode);
 
 			
 			
+		}
+		size--;
+		return m.root();
 	}
-	return m.getValue().root();
-    }
-    public int compareNodes(DNode<Tree<E>> a, DNode<Tree<E>> b){
-	return comp.compare(a.getValue().root(), b.getValue().root());
-    }
-    public void decreaseKey(Tree<E> tree, E val){
-	if(tree.parent() == null){
-	    tree.setRoot(val);
-	    DNode<Tree<E>> node = head.getNext();
-	    if(compareNodes(head, min) < 0) min = head;
-	    while(node != head){
-		if(compareNodes(node, min) < 0) min = node;
-		node = node.getNext();
-	    }
-	}else{
-	    tree.setRoot(val);
-	    if(comp.compare(val, tree.parent.root()) < 0){
-		cut(tree);
+	public int compareNodes(Tree<E> a, Tree<E> b){
+		return comp.compare(a.root(), b.root());
+	}
+	private Tree<E> find(E val){
+		Tree<E> node = head.getNext();
+		while(node != head){
+			Tree<E> t = find(node, val);
+			node = node.getNext();
+		}
+				
+	}
+	private Tree<E> find(Tree<E> parent, E val){
+		if(parent.root().equals(val)) return parent;
+		for 
 		
-	    }
 	}
-    }
-    public void cut(Tree<E> tree){
-	Tree<E> parent = tree.parent();
-	parent.children.remove(tree);
-	tree.unmark();
-	insert(tree);
-	tree.setParent(null);
-	if(!parent.marked() && parent.parent() != null){
-	    parent.mark();
-	}else{
-	    cut(parent);
+	public void decreaseKey(Tree<E> tree, E val){
+		locations.remove(tree.root());
+		tree.setRoot(val);
+		if(tree.parent() == null){
+			Tree<E> node = head.getNext();
+			if(compareNodes(head, min) < 0) min = head;
+			while(node != head){
+				if(compareNodes(node, min) < 0) min = node;
+				node = node.getNext();
+			}
+		}else{
+			if(comp.compare(val, tree.parent().root()) < 0){
+				cut(tree);
+			}
+		}
+		locations.put(val, tree.root());
 	}
-    }
-    public void print(){
-	System.out.println("--------------");
-	DNode<Tree<E>> node = head.getNext();
-	System.out.println(head);
-	while(node != head){
-	    System.out.println(node);
-	    node = node.getNext();
+	public void decreaseKey(E val, E newVal){
+		decreaseKey(locations.get(val), newVal);
 	}
-	System.out.println("--------------");
-    }
-    public static void main(String[] args){
-	FibonacciHeap<Integer> fib = new FibonacciHeap<Integer>();
-	for(int i = 0; i < 10; i ++){
-	    fib.add(i);
+	public void cut(Tree<E> tree){
+		Tree<E> parent = tree.parent();
+		parent.children().remove(tree);
+		tree.unmark();
+		insert(tree);
+		tree.setParent(null);
+		if(!parent.marked() && parent.parent() != null){
+			parent.mark();
+		}else{
+			cut(parent);
+		}
 	}
-	fib.print();
-	//System.out.println(fib.min());
-	System.out.println(fib.deleteMin());
-	fib.print();
-    }
+	public void print(){
+		System.out.println("--------------");
+		Tree<E> node = head.getNext();
+		System.out.println(head);
+		while(node != head){
+			System.out.println(node);
+			node = node.getNext();
+		}
+		System.out.println("--------------");
+	}
+	public static void main(String[] args){
+		FibonacciHeap<Integer> fib = new FibonacciHeap<Integer>();
+		for(int i = 0; i < 10; i ++){
+			fib.add(i);
+		}
+		fib.print();
+		//System.out.println(fib.min());
+		System.out.println(fib.deleteMin());
+		fib.print();
+	}
 }
--- APCS/Graphs/Tree.java
+++ APCS/Graphs/Tree.java
@@ -1,77 +1,77 @@
 import java.util.*;
 public class Tree<E>{
-    private E root;
-    private ArrayList<Tree<E>> children = new ArrayList<Tree<E>>();
-    private int treeRank = 1;
-    private Tree<E> parent;
-    private boolean marked = false;
-    private Tree<E> previous, next;
-    public Tree(E root){
-	this(root, null, null);
-    }
-    public Tree(E root, Tree<E> next, Tree<E> previous){
-	this.root = root;
-	parent = null;
-	this.next = next;
-	this.previous = previous;
-    }
-    public void addChild(E child){
-	addChild(new Tree<E>(child));
-    }
-    public void addChild(Tree<E> child){
-	children.add(child);
-	treeRank += child.treeRank();
-	child.setParent(this);
-    }
-    public int rank(){
-	return children.size();
-    }
-    public int treeRank(){
-	return treeRank;
-    }
-    public E root(){
-	return root;
-    }
-    public void setRoot(E val){
-	root = val;
-    }
-    public Tree<E> parent(){
-	return parent;
-    }
-    public void setParent(Tree<E> t){
-	parent = t;
-    }
-    public ArrayList<Tree<E>> children(){
-	return children;
-    }
-    public boolean marked(){
-	return marked;
-    }
-    public void mark(){
-	marked = true;
-    }
-    public void unmark(){
-	marked = false;
-    }
-    public Tree<E> getNext(){
-	return next;
-    }
-    public Tree<E> getPrevious(){
-	return previous;
-    }
-    public void setNext(Tree<E> t){
-	next = t;
-    }
-    public void setPrevious(Tree<E> t){
-	previous = t;
-    }
-    public String toString(){
-	String s = root + " : [ ";
-	for(Tree<E> child : children){
-	    s += child + ", ";
+	private E root;
+	private ArrayList<Tree<E>> children = new ArrayList<Tree<E>>();
+	private int treeRank = 1;
+	private Tree<E> parent;
+	private boolean marked = false;
+	private Tree<E> previous, next;
+	public Tree(E root, Tree<E> next, Tree<E> previous){
+		this.root = root;
+		parent = null;
+		this.next = next;
+		this.previous = previous;
 	}
-	s += "]";
-	return s.replace(", ]", " ]").replace(" : [ ]", "");
-    }
+	public Tree(E root){
+		this(root, null, null);
+	}
+	public void addChild(E child){
+		addChild(new Tree<E>(child));
+	}
+	public void addChild(Tree<E> child){
+		children.add(child);
+		treeRank += child.treeRank();
+		child.setParent(this);
+	}
+	public int rank(){
+		return children.size();
+	}
+	public int treeRank(){
+		return treeRank;
+	}
+	public E root(){
+		return root;
+	}
+	public void setRoot(E val){
+		root = val;
+	}
+	public Tree<E> parent(){
+		return parent;
+	}
+	public void setParent(Tree<E> t){
+		parent = t;
+	}
+	public ArrayList<Tree<E>> children(){
+		return children;
+	}
+	public boolean marked(){
+		return marked;
+	}
+	public void mark(){
+		marked = true;
+	}
+	public void unmark(){
+		marked = false;
+	}
+	public Tree<E> getNext(){
+		return next;
+	}
+	public Tree<E> getPrevious(){
+		return previous;
+	}
+	public void setNext(Tree<E> t){
+		next = t;
+	}
+	public void setPrevious(Tree<E> t){
+		previous = t;
+	}
+	public String toString(){
+		String s = root + " : [ ";
+		for(Tree<E> child : children){
+			s += child + ", ";
+		}
+		s += "]";
+		return s.replace(", ]", " ]").replace(" : [ ]", "");
+	}
 	
 }
