0d704728c159e1813e9a30a8c70bb24022aeff95
Theadora Williams <theadorabledeveloper@gmail.com>
2019-05-13 21:21:20 -0400

    Homework and implemented all priority queue methods for the fibonacci heap

---
+++ APCS/BinaryTrees/IntBST.java
@@ -0,0 +1,106 @@
+public class IntBST{
+
+    // instance variables
+    private TreeNode<Integer> root;
+    private int size; 
+
+    private static class TreeNode<Integer> {
+	private Integer value;
+	private TreeNode<Integer> left, right;
+
+	public TreeNode(Integer value, TreeNode<Integer> left,
+			TreeNode<Integer> right){
+	    this.value = value;
+	    this.left = left;
+	    this.right = right;
+	}
+
+
+	// accessors
+	public Integer getValue() { return value;}
+	public TreeNode<Integer> getLeft() {return left;}
+	public TreeNode<Integer> getRight() {return right;}
+	
+	// mutators
+	public void setLeft(TreeNode<Integer> n) { left = n;}
+	public void setRight(TreeNode<Integer> n) { right = n;}
+
+    }
+
+    public IntBST(){
+	root = null;
+	size = 0;
+    }
+
+    // Adds x into the IntBST as a leaf.
+    public void add(Integer x){
+	root = add(x,root);
+	size++;
+    }
+    
+    private TreeNode<Integer> add(Integer x, TreeNode<Integer> rt){
+	if (rt == null)
+	    rt = new TreeNode<Integer>(x,null,null);
+	else if (x <= rt.getValue())
+	    rt.setLeft(add(x,rt.getLeft()));
+	else 
+	    rt.setRight(add(x,rt.getRight()));
+	return rt;
+    }
+
+    public void inorder(){
+	inorder(root);
+	System.out.println();
+    }
+    private void inorder(TreeNode<Integer> rt){
+	if (rt != null){
+	    inorder(rt.getLeft());
+	    System.out.print(rt.getValue() + " ");
+	    inorder(rt.getRight());
+
+	}
+    }
+	public void preorder(){
+	preorder(root);
+	System.out.println();
+    }
+    private void preorder(TreeNode<Integer> rt){
+	if (rt != null){
+	    System.out.print(rt.getValue() + " ");
+		preorder(rt.getLeft());
+	    preorder(rt.getRight());
+
+	}
+    }
+	public void postorder(){
+	postorder(root);
+	System.out.println();
+    }
+    private void postorder(TreeNode<Integer> rt){
+	if (rt != null){
+	    postorder(rt.getLeft());
+	    postorder(rt.getRight());
+	    System.out.print(rt.getValue() + " ");
+
+	}
+    }
+
+
+    public static void main(String [] args){
+	IntBST bst = new IntBST();
+	bst.add(5);
+	bst.add(7);
+	bst.add(4);
+	bst.add(2);
+	bst.add(6);
+	bst.add(3);
+	bst.add(8);
+	bst.add(1);
+	bst.add(9);
+	bst.inorder();
+	bst.preorder();
+	bst.postorder();
+    }
+
+
+}
