878562db8a6493d55328c24fc57f243348c98267
Theadora Williams <theadorabledeveloper@gmail.com>
2019-05-31 09:15:53 -0400

    Added a remove function.

--- APCS/Maps/HashST.java
+++ APCS/Maps/HashST.java
@@ -4,113 +4,129 @@ import java.util.Set;
 
 public class HashST<Key,Value>{
 
-	private static  class Node{
-		private Object key;
-		private Object value;
-		private Node next;
+    private static  class Node{
+	private Object key;
+	private Object value;
+	private Node next;
 
-		public Node(Object key, Object value, Node next){
-			this.key = key;
-			this.value = value;
-			this.next = next;
-		}
-		public String toString(){
-			return key + " : " + value;
-		}
-		public Node next(){
-			return next;
-		}
-		public void setNext(Node n){
-			next = n;
-		}
-		public Object key(){
-			return key;
-		}
-		public Object value(){
-			return value;
-		}
-		public void setValue(Object v){
-			value = v;
-		}
-
+	public Node(Object key, Object value, Node next){
+	    this.key = key;
+	    this.value = value;
+	    this.next = next;
+	}
+	public String toString(){
+	    return key + " : " + value;
+	}
+	public Node next(){
+	    return next;
+	}
+	public void setNext(Node n){
+	    next = n;
+	}
+	public Object key(){
+	    return key;
+	}
+	public Object value(){
+	    return value;
+	}
+	public void setValue(Object v){
+	    value = v;
 	}
 
-	// instance variables
-	private int m;   // # of buckets
-	private Node[] buckets;
-	private HashSet<Key> keys;
+    }
+
+    // instance variables
+    private int m;   // # of buckets
+    private Node[] buckets;
+    private HashSet<Key> keys;
 	
 	
 	
-	public HashST(int capacity){
-		m = capacity;
-		buckets = new Node[m];
-		keys = new HashSet<Key> ();
-	}
+    public HashST(int capacity){
+	m = capacity;
+	buckets = new Node[m];
+	keys = new HashSet<Key> ();
+    }
 	
-	public int hash(Object key){
-		return Math.abs(key.hashCode()) % m;
-	}
+    public int hash(Object key){
+	return Math.abs(key.hashCode()) % m;
+    }
 	
-	public int size(){
-			return keys.size();
+    public int size(){
+	return keys.size();
+    }
+    public boolean containsKey(Key key){
+	return keys.contains(key);
+    }
+    public Set<Key> getKeys(){
+	return keys;
+    }
+    private Node getNode(Key key){
+	Node node = buckets[hash(key)];
+	while(node != null){
+	    if(node.key().equals(key)) return node;
+	    node = node.next();
 	}
-	public boolean containsKey(Key key){
-		return keys.contains(key);
+	return null;
+    }
+    public Value get(Key key){
+	Node n = getNode(key);
+	if (n == null) return null;
+	return (Value) n.value();
+    }
+    public Value put(Key key, Value value){
+	Value old = null;
+	Node n = getNode(key);
+	if (containsKey(key)){
+	    old = (Value) n.value();
+	    n.setValue(value);
+	} else{
+	    keys.add(key);
+	    buckets[hash(key)] = new Node(key, value, buckets[hash(key)]);
 	}
-	public Set<Key> getKeys(){
-		return keys;
+	return old;
+    }
+    public void printTable(){
+	for(int i = 0; i < m; i++){
+	    System.out.print("[" + i + "] : ");
+	    Node node = buckets[i];
+	    while(node != null){
+		System.out.print(node + " ");
+		node = node.next();
+	    }
+	    System.out.println();
 	}
-	private Node getNode(Key key){
-		Node node = buckets[hash(key)];
-		while(node != null){
-			if(node.key().equals(key)) return node;
-			node = node.next();
-		}
-		return null;
-	}
-	public Value get(Key key){
-		Node n = getNode(key);
-		if (n == null) return null;
-		return (Value) n.value();
-	}
-	public Value put(Key key, Value value){
-		Value old = null;
-		Node n = getNode(key);
-		if (containsKey(key)){
-			old = (Value) n.value();
-			n.setValue(value);
-		} else{
-			keys.add(key);
-			buckets[hash(key)] = new Node(key, value, buckets[hash(key)]);
-		}
-		return old;
-	}
-	public void printTable(){
-		for(int i = 0; i < m; i++){
-			System.out.print("[" + i + "] : ");
-			Node node = buckets[i];
-			while(node != null){
-				System.out.print(node + " ");
-				node = node.next();
-			}
-			System.out.println();
-		}
 		
+    }
+    public void remove(Key key){
+	if(!containsKey(key)) return;
+	Node node = buckets[hash(key)];
+	Node trailer = node;
+	while(node.next() != null){
+	    if(node.key().equals(key)){
+		trailer.setNext(node.next());
+		keys.remove(key);
+		return;
+	    }
+	    trailer = node;
+	    node = node.next();
 	}
+	
+	
+    }
     
 
 	
-	public static void main(String [] args){
-		HashST<String, Integer> d = new HashST<String, Integer> (13);
-		for(String s : args){
-		    if(! d.containsKey(s))
-			d.put(s, 0);
-		    d.put(s, d.get(s) + 1);
+    public static void main(String [] args){
+	HashST<String, Integer> d = new HashST<String, Integer> (13);
+	for(String s : args){
+	    if(! d.containsKey(s))
+		d.put(s, 0);
+	    d.put(s, d.get(s) + 1);
 		    
-		}
-		d.printTable();
 	}
+	d.printTable();
+    }
 
 	
 	
