8c93559a51996b415075c05b218db70595cf0367
Theadora Williams <theadorabledeveloper@gmail.com>
2019-06-07 09:27:19 -0400

    More markov work.

--- APCS/FinalProject/LanguageModeling/TextGenerator.java
+++ APCS/FinalProject/LanguageModeling/TextGenerator.java
@@ -1,27 +1,37 @@
 public class TextGenerator{
-	private LanguageModeler model;
-	private String text = "";
-	private int length;
-	private int kOrder;
-	public TextGenerator(LanguageModeler lm, int l, int k){
-		length = l;
-		model = lm;
-		kOrder = k;
+    private LanguageModeler model;
+    private String origText = "";
+    private String text = "";
+    private int length;
+    private int kOrder;
+    public TextGenerator(LanguageModeler lm, int l, int k, String t){
+	length = l;
+	model = lm;
+	kOrder = k;
+	origText = t;
+    }
+    public void print(){
+	System.out.println(text);
+    }
+    public void generateText(){
+	text = origText.substring(kOrder);
+	while(text.length() < length){
+	    print();
+	    text += model.map.get(text.substring(text.length() + 1- kOrder, text.length())).random();
 	}
-	public void print(){
-		System.out.println(text);
-	}
-	public void generateText(){
-		text = lm.map.get("").random();
-		while(text.length() < length){
-			
-		}
-	}
-	public static void main(String [] args){
-		int kOrder = Integer.parseInt(args[0]);
-		String text = getTextFromStdin();
-		LanguageModeler lm = new LanguageModeler(kOrder, text);
-		
-	}	
+    }
+    public static void main(String [] args){
+	int kOrder = Integer.parseInt(args[0]);
+	int length = Integer.parseInt(args[1]);
+	String text = LanguageModeler.getTextFromStdin();
+	System.out.println("Original text : ");
+	System.out.println(text);
+	LanguageModeler lm = new LanguageModeler(kOrder, text);
+	TextGenerator g = new TextGenerator(lm, length, kOrder, text);
+	g.generateText();
+	System.out.println("New text : ");
+	g.print();
 	
+    }	
+    
 }
